269b059f00ef3ed23c192f72fa56d8ea47fcda99
where where <qq2518766683@outlook.com>
2026-03-29 14:07:03 +0800

    Add split-view demo, styles

--- dev/index.ts
+++ dev/index.ts
@@ -8,6 +8,9 @@ import { printTree } from './print-lezer-tree';
 import { oneDark } from '@codemirror/theme-one-dark';
 
 const doc = '';
+const syntax = document.createElement('pre');
+syntax.className = 'ͼo';
+document.getElementById('syntax')!.appendChild(syntax);
 
 new EditorView({
     state: EditorState.create({
@@ -18,15 +21,14 @@ new EditorView({
             oneDark,
             keymap.of([indentWithTab]),
             indentUnit.of('    '),
-            EditorView.lineWrapping,
             EditorView.updateListener.of(e => {
                 if (e.docChanged) {
                     const doc = e.state.doc.toString();
-                    console.log(printTree(parser.parse(doc), doc));
+                    syntax.textContent = printTree(parser.parse(doc), doc);
                 }
             })],
     }),
     parent: document.querySelector('#editor')!,
 });
 
-console.log(printTree(parser.parse(doc), doc));
+syntax.textContent = printTree(parser.parse(doc), doc);
